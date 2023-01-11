9e1fb31bda66378ad8e50f511dfba09d8f6d2124
MyFrei <iarov912@gmail.com>
2023-01-11 23:58:12 +0300

    init commin

--- ClientProfileService/src/main/java/org/client/MainApp.java
+++ ClientProfileService/src/main/java/org/client/MainApp.java
@@ -1,18 +1,14 @@
 package org.client;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * ClientProfileService!
  *
  */
 @SpringBootApplication
-@EnableSwagger2
 @EnableWebMvc
 public class MainApp
 {
