098555ecd6a7a9f8903470ffb0c451238f4ddde9
Илья Аров <iarov912@gmail.com>
2023-02-10 15:04:47 +0000

    Revert "Created entity, repository, service commit"
    This reverts commit b3091eb154130d55abd295027841ee94744f5445

--- ClientProfileService/src/main/java/org/client/service/impl/IndividualServiceImpl.java
+++ ClientProfileService/src/main/java/org/client/service/impl/IndividualServiceImpl.java
@@ -1,47 +1,22 @@
 package org.client.service.impl;
 
 import org.client.dto.IndividualDto;
-import org.client.model.Address;
-import org.client.model.Individual;
-import org.client.model.Wallet;
-import org.client.repository.AddressRepository;
-import org.client.repository.IndividualRepository;
-import org.client.repository.WalletRepository;
 import org.client.service.IndividualService;
-import org.client.utils.MappingUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-@Service
-@Transactional(readOnly = true)
 public class IndividualServiceImpl implements IndividualService {
 
-    private final IndividualRepository individualRepository;
-    private final MappingUtils mappingUtils;
-
-    @Autowired
-    public IndividualServiceImpl(IndividualRepository individualRepository, MappingUtils mappingUtils) {
-        this.individualRepository = individualRepository;
-        this.mappingUtils = mappingUtils;
-    }
-
+    private Map<String, IndividualDto> persist = new HashMap<>();
     @Override
-    @Transactional
     public void addClient(String icp, String name) {
-        Individual individual = new Individual();
-        individual.setIcp(icp);
-        individual.setUuid(UUID.randomUUID().toString());
-        individual.setName(name);
-        individualRepository.save(individual);
+        persist.put(icp, IndividualDto.builder().icp(icp).uuid(UUID.randomUUID().toString()).name(name).build());
     }
 
     @Override
     public IndividualDto getClient(String icp) {
-        return mappingUtils.mapToIndividualDto(individualRepository.findByIcp(icp));
+        return persist.get(icp);
     }
 }
